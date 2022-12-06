55a997a7eb3a60f5192d8b73cae1a23cf6464625
Noelia <npergon914@g.educaand.es>
2022-12-06 13:58:02 +0100

    terminado

--- src/main/java/ut05estructurasdealmacenamiento/UT0506.java
+++ src/main/java/ut05estructurasdealmacenamiento/UT0506.java
@@ -4,6 +4,7 @@
  */
 package ut05estructurasdealmacenamiento;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -13,23 +14,49 @@ import java.util.Arrays;
 public class UT0506 {
 
     /**
+     * Hacer un programa que determine si dos arrays son iguales. Probarlo con
+     * los arrays [1,2,3] y [1,2,3], [1,2,3,4] y [1,2,3] , [1,2,3] y [1,2,2].
+     * Este ejercicio hay que hacerlo de dos formas: la primera, usando la clase
+     * Arrays; la segunda, creando un método que vaya comparando los valores de
+     * los arrays y devuelva true si todos son iguales o false en caso
+     * contrario.
      *
      */
     public static void main(String[] args) {
 
         int array1[] = {1, 2, 3};
-        int array2[] = {1, 2, 3};
-        int array3[] = {1, 2, 3, 4};
-        int array4[] = {1, 2, 2};
+        int copiaArray1[] = Arrays.copyOf(array1, 3);
+        int array2[] = {1, 2, 3, 4};
+        int array3[] = {1, 2, 2};
 
-        System.out.println(Arrays.equals(array1, array2)
+        System.out.println(Arrays.equals(array1, copiaArray1)
                 ? "Son iguales" : "Son diferentes");
-        System.out.println(Arrays.equals(array3, array1) 
+        System.out.println(Arrays.equals(array2, array1)
                 ? "Son iguales" : "Son diferentes");
-        System.out.println(Arrays.equals(array1, array4) 
+        System.out.println(Arrays.equals(array1, array3)
                 ? "Son iguales" : "Son diferentes");
         
-        
+        System.out.println(miEquals(array1, copiaArray1)
+                ? "Son iguales" : "Son diferentes");
+        System.out.println(miEquals(array2, array1)
+                ? "Son iguales" : "Son diferentes");
+        System.out.println(miEquals(array1, array3)
+                ? "Son iguales" : "Son diferentes");
+
+    }
+
+    private static boolean miEquals(int array1[], int array2[]) {
+
+        if (array1.length == array2.length) {
+            for (int i = 0; i < array1.length; i++) {
+                if (array1[i] != array2[i]) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
 
     }
 
--- src/main/java/ut05estructurasdealmacenamiento/UT0507.java
+++ src/main/java/ut05estructurasdealmacenamiento/UT0507.java
@@ -4,6 +4,9 @@
  */
 package ut05estructurasdealmacenamiento;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author noelia
@@ -11,10 +14,51 @@ package ut05estructurasdealmacenamiento;
 public class UT0507 {
 
     /**
-     * 
+     * Escribir un programa que lea del teclado el tamaño de dos arrays de
+     * enteros (el mismo tamaño para los dos arrays). El programa inicializará
+     * los arrays con números aleatorios entre 1 y 100. Existirá un método
+     * llamado multiplicar que recibirá los dos arrays e irá multiplicando los
+     * elementos dos a dos, empezando por los elementos que ocupan la posición
+     * cero, luego la uno, etc, guardando el resultado en otro array, que será
+     * devuelto por el método. Además existirá un método para imprimir por
+     * pantalla los valores de los arrays.
      */
     public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce el tamaño de los arrays");
+        int tamañoArray = teclado.nextInt();
+        int[] array1 = new int[tamañoArray];
+        int[] array2 = new int[tamañoArray];
+
+        Random aleatorio = new Random();
+
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = aleatorio.nextInt(100) + 1;
+            array2[i] = aleatorio.nextInt(100) + 1;
+        }
+        
+        int[] arrayMultiplicacion = multiplicar(array1, array2);
+        
+        imprimirArrays(array1, array2, arrayMultiplicacion);
+
+    }
+
+    private static int[] multiplicar(int[] array1, int[] array2) {
+
+        int[] arrayMultiplicacion = new int[array1.length];
+        for (int i = 0; i < array1.length; i++) {
+            arrayMultiplicacion[i] = array1[i] * array2[i];
+        }
+        return arrayMultiplicacion;
+    }
+
+    private static void imprimirArrays(int[] array1, int[] array2, int[] arrayMultiplicacion) {
+
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println(array1[i] + " x " + array2[i] + " = " + arrayMultiplicacion[i]);
+        }
         
     }
-    
+
 }

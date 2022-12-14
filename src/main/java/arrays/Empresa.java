02d2f474aa27afb115536d136f667564fb093c48
Noelia <perezgonzaleznoelia08@gmail.com>
2022-12-14 14:40:10 +0100

    ,,

--- src/main/java/arrays/Empresa.java
+++ src/main/java/arrays/Empresa.java
@@ -5,9 +5,16 @@
 package arrays;
 
 /**
- *
+ * clase empresa cif nombre tiene catalogo vehiculos, catalogo clientes,
+ * catalogo alquileres metodos---- registrar cliente registrar vehiculos buscar
+ * un cliente a partir del nif buscar vehiculo a partir del bastidor to string
+ * tocho con cif, nombre y los catalogos
+ * 
  * @author noelia
  */
 public class Empresa {
+    
+    private String nombre;
+    private String cif;
     
 }

cb6a1422840b6acc38e1a1d92cf2b150409bbb42
Noelia <npergon914@g.educaand.es>
2022-12-17 13:00:50 +0100

    ejers

---
+++ src/main/java/ut05estructurasdealmacenamiento/UT0515.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ut05estructurasdealmacenamiento;
+
+/**
+ *
+ * @author FX506
+ */
+public class UT0515 {
+
+    /**
+     * Escribe una clase LanzarMoneda que tenga como único atributo un array de
+     * booleanos de tamaño 1000. La clase tendrá los siguientes métodos: 
+     * 
+     * a. un método para rellenar el array, de forma que simule 1000 lanzamientos de
+     * una moneda. El método debe almacenar el resultado de cada tirada en el array. 
+     * 
+     * b.un método para saber cuántas “caras” salieron después de los
+     * lanzamientos.
+     * 
+     * c. un método para saber cuántas “cruces” salieron después
+     * de los lanzamientos. 
+     * 
+     * d. un método para imprimir el resultado de los 1000
+     * lanzamientos, de forma que aparezca el número de lanzamiento y el
+     * resultado (cara o cruz)
+     *
+     */
+    public static void main(String[] args) {
+        
+        
+
+    }
+
+}
---
+++ src/main/java/ut05estructurasdealmacenamiento/UT0523.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ut05estructurasdealmacenamiento;
+
+/**
+ *
+ * @author FX506
+ */
+public class UT0523 {
+
+    /**
+     * Se desea manejar dos listas (ArrayList) de Integer. La cantidad de
+     * elementos de la lista 1 debe ser un número aleatorio entre 5 y 15. La
+     * cantidad de elementos de la lista 2 debe ser un número aleatorio entre 10
+     * y 20. El rango de los números que serán agregados a la lista DEBE estar
+     * entre 50 y 100. Se pide construir una aplicación en Java que permita
+     * cumplir con la siguiente funcionalidad: 
+     * 
+     * Imprimir las listas que han sido generadas. Usa for-each. 
+     * 
+     * Crear una tercera lista que contenga los elementos que están en la lista 1 
+     * y NO ESTÁN en la lista 2 
+     * 
+     * Imprimir la tercera lista. 
+     * 
+     * Crear una cuarta lista que contenga los elementos de la
+     * lista 1 que son pares y los elementos de la lista 2 que son impares
+     * 
+     * Imprimir la cuarta lista. Usa una expresión lambda.
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+
+}

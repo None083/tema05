0cbe3290aa81a40477237f7814c921ed282b0e9f
Noelia <perezgonzaleznoelia08@gmail.com>
2023-01-13 11:08:56 +0100

    ..

---
+++ src/main/java/ut05estructurasdealmacenamiento/UT0512.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ut05estructurasdealmacenamiento;
+
+/**
+ *
+ * @author noelia
+ */
+public class UT0512 {
+
+    /**
+     * Declara e inicializa una matriz de caracteres de 2x3, con los valores 
+     * que tú quieras. Implementa un método, en la misma clase que el 
+     * método main(), que reciba una matriz de caracteres y la imprima por consola.
+
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
---
+++ src/main/java/ut05estructurasdealmacenamiento/UT0514.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ut05estructurasdealmacenamiento;
+
+/**
+ *
+ * @author noelia
+ */
+public class UT0514 {
+
+    /**
+    Escribe un programa que dada una matriz de 3x3 enteros y realice las siguientes funciones:
+    Rellenar aleatoriamente todas las casillas de la matriz con números enteros entre 1 y 100
+    Calcular la media de todos los valores
+    Calcular el valor mínimo y el máximo de todos los valores
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}

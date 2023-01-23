dd2443bc53fa52d0aae798a68bea063b37ca1478
Noelia <perezgonzaleznoelia08@gmail.com>
2023-01-23 14:33:49 +0100

    230123

---
+++ src/main/java/matrices/Vecinas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package matrices;
+
+/**
+ *
+ * @author noelia
+ */
+public class Vecinas {
+
+    /**
+     * matriz nxn
+     * coordenada - fila, col
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/main/java/matrices/diagonal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package matrices;
+
+/**
+ *
+ * @author noelia
+ */
+public class diagonal {
+
+    /**
+     * matriz nxn
+     * recorrer diagonal principal
+     * recorrer diagonal secundaria
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

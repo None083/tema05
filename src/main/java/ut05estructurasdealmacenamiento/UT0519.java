8176db96ffd1a72d476d105daa08de3df17e2ccf
Noelia <perezgonzaleznoelia08@gmail.com>
2023-01-23 14:27:31 +0100

    .

---
+++ src/main/java/ut05estructurasdealmacenamiento/UT0519.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ut05estructurasdealmacenamiento;
+
+/**
+ *
+ * @author noelia
+ */
+public class UT0519 {
+
+    /**
+     * Crear un programa que, mediante un menú, permita reservar o cancelar 
+     * asientos de un avión, así como mostrar qué asientos están ocupados 
+     * y libres actualmente. El avión tendrá 25 filas de 4 asientos cada una.
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}

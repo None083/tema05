b6540127e2851525d0b33f6a6af0aa3b85221768
Noelia <npergon914@g.educaand.es>
2023-01-25 00:55:59 +0100

    250123

--- src/main/java/matrices/Vecinas.java
+++ src/main/java/matrices/Vecinas.java
@@ -4,6 +4,9 @@
  */
 package matrices;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author noelia
@@ -11,11 +14,58 @@ package matrices;
 public class Vecinas {
 
     /**
-     * matriz nxn
-     * coordenada - fila, col
+     * matriz nxn coordenada - fila, col
      */
     public static void main(String[] args) {
+
+        Random random = new Random();
+        int[][] matriz1 = new int[3][3];
+
+        for (int i = 0; i < matriz1.length; i++) {
+            for (int j = 0; j < matriz1[i].length; j++) {
+                matriz1[i][j] = random.nextInt(21);
+            }
+        }
         
+         for (int i = 0; i < matriz1.length; i++) {
+            for (int j = 0; j < matriz1[i].length; j++) {
+                System.out.print(matriz1[i][j] + " ");
+            }
+             System.out.println("");
+        }
+        ArrayList<Integer> array1 = buscarVecinos(matriz1, 0, 0);
+        for (int i = 0; i < array1.size(); i++) {
+            System.out.println(array1.get(i));
+        }
     }
-    
+
+    private static ArrayList<Integer> buscarVecinos(int[][] matriz, int fila, int columna) {
+        ArrayList<Integer> array1 = new ArrayList<>();
+        if (fila - 1 >= 0 && columna - 1 >= 0) {
+            array1.add(matriz[fila - 1][columna - 1]);
+        }
+        if (fila - 1 >= 0) {
+            array1.add(matriz[fila - 1][columna]);
+        }
+        if (fila - 1 >= 0 && columna + 1 < matriz[fila].length) {
+            array1.add(matriz[fila - 1][columna + 1]);
+        }
+        if (columna - 1 >= 0) {
+            array1.add(matriz[fila][columna - 1]);
+        }
+        if (columna + 1 < matriz[fila].length) {
+            array1.add(matriz[fila][columna + 1]);
+        }
+        if (fila + 1 < matriz.length && columna - 1 >= 0) {
+            array1.add(matriz[fila + 1][columna - 1]);
+        }
+        if (fila + 1 < matriz.length) {
+            array1.add(matriz[fila + 1][columna]);
+        }
+        if (fila + 1 < matriz.length && columna + 1 < matriz[fila].length) {
+            array1.add(matriz[fila + 1][columna + 1]);
+        }
+        return array1;
+    }
+
 }
--- src/main/java/ut05estructurasdealmacenamiento/UT0519.java
+++ src/main/java/ut05estructurasdealmacenamiento/UT0519.java
@@ -4,6 +4,9 @@
  */
 package ut05estructurasdealmacenamiento;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author noelia
@@ -11,12 +14,130 @@ package ut05estructurasdealmacenamiento;
 public class UT0519 {
 
     /**
-     * Crear un programa que, mediante un menú, permita reservar o cancelar 
-     * asientos de un avión, así como mostrar qué asientos están ocupados 
-     * y libres actualmente. El avión tendrá 25 filas de 4 asientos cada una.
+     * Crear un programa que, mediante un menú, permita reservar o cancelar
+     * asientos de un avión, así como mostrar qué asientos están ocupados y
+     * libres actualmente. El avión tendrá 25 filas de 4 asientos cada una.
      */
     public static void main(String[] args) {
-        
+
+        Scanner teclado = new Scanner(System.in);
+        char[][] avion = new char[25][4];
+
+        for (int i = 0; i < avion.length; i++) {
+
+            for (int j = 0; j < avion[i].length; j++) {
+                avion[i][j] = 'O';
+            }
+        }
+
+        int opcion = 0;
+        do {
+            System.out.println("""
+                           -----------------------------------------------------
+                                            ESCOJA UNA OPCIÓN
+                           -----------------------------------------------------
+                                       1 - Reservar asiento
+                                       2 - Cancelar reserva
+                                       3 - Ver asientos
+                                       4 - Salir
+                           -----------------------------------------------------
+                           """);
+            try {
+                opcion = teclado.nextInt();
+            } catch (InputMismatchException ime) {
+                opcion = 5;
+                teclado.nextLine();
+            }
+
+            int fila;
+            int columna;
+            switch (opcion) {
+                case 1:
+                    do {
+                        System.out.println("Introduzca el número de la fila entre 1 - 25");
+                        try {
+                            fila = teclado.nextInt() - 1;
+                        } catch (InputMismatchException ime) {
+                            fila = 25;
+                            teclado.nextLine();
+                        }
+
+                    } while (fila < 0 || fila > 24);
+                    do {
+                        System.out.println("Introduzca el número del asiento entre 1 - 4");
+                        try {
+                            columna = teclado.nextInt() - 1;
+                        } catch (InputMismatchException ime) {
+                            columna = 5;
+                            teclado.nextLine();
+                        }
+                    } while (columna < 0 || columna > 3);
+                    reservarAsiento(avion, fila, columna);
+                    break;
+                case 2:
+                    do {
+                        System.out.println("Introduzca el número de la fila entre 1 - 25");
+                        try {
+                            fila = teclado.nextInt() - 1;
+                        } catch (InputMismatchException ime) {
+                            fila = 26;
+                            teclado.nextLine();
+                        }
+                    } while (fila < 0 || fila > 24);
+                    do {
+                        System.out.println("Introduzca el número del asiento entre 1 - 4");
+                        try {
+                            columna = teclado.nextInt() - 1;
+                        } catch (InputMismatchException ime) {
+                            columna = 5;
+                            teclado.nextLine();
+                        }
+                    } while (columna < 0 || columna > 3);
+                    cancelarReserva(avion, fila, columna);
+                    break;
+                case 3:
+                    imprimirAvion(avion);
+                    break;
+                case 4:
+                    System.out.println("Hasta luegui <3");
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
+        } while (opcion != 4);
+
     }
-    
+
+    private static void imprimirAvion(char[][] avion) {
+
+        for (int i = 0; i < avion.length; i++) {
+
+            for (int j = 0; j < avion[i].length; j++) {
+                System.out.print(avion[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    private static void reservarAsiento(char[][] avion, int fila, int columna) {
+
+        if (avion[fila][columna] == 'O') {
+            avion[fila][columna] = 'X';
+            System.out.println("Ha reservado su asiento");
+        } else {
+            System.out.println("Asiento no disponible");
+        }
+    }
+
+    private static void cancelarReserva(char[][] avion, int fila, int columna) {
+
+        if (avion[fila][columna] == 'X') {
+            avion[fila][columna] = 'O';
+            System.out.println("Ha cancelado su reserva");
+        } else {
+            System.out.println("Este asiento no está reservado");
+        }
+    }
+
 }

63bcbbe5e85310b6c2af47746aa8304de0c9d8e5
Noelia <perezgonzaleznoelia08@gmail.com>
2023-01-26 12:48:19 +0100

    jkcdhudcklhsailufh

---
+++ src/main/java/ordenacionbusqueda/Libro.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ordenacionbusqueda;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ *
+ * @author noelia
+ */
+public class Libro implements Comparable<Libro>{
+    
+    private String nombre;
+    private String ISBN;
+    private String categoría;
+    private String autor;
+    private String color;
+    private static int contador = 0;
+
+    public Libro(String nombre, String categoría, String autor, String color) {
+        this.nombre = nombre;
+        this.categoría = categoría;
+        this.autor = autor;
+        this.color = color;
+        contador++;
+        this.ISBN = String.valueOf(contador);
+        
+    }
+
+    public Libro() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getCategoría() {
+        return categoría;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCategoría(String categoría) {
+        this.categoría = categoría;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" + "nombre=" + nombre + ", ISBN=" + ISBN + ", categor\u00eda=" + categoría + ", autor=" + autor + ", color=" + color + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 31 * hash + Objects.hashCode(this.nombre);
+        hash = 31 * hash + Objects.hashCode(this.ISBN);
+        hash = 31 * hash + Objects.hashCode(this.categoría);
+        hash = 31 * hash + Objects.hashCode(this.autor);
+        hash = 31 * hash + Objects.hashCode(this.color);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Libro other = (Libro) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.ISBN, other.ISBN)) {
+            return false;
+        }
+        if (!Objects.equals(this.categoría, other.categoría)) {
+            return false;
+        }
+        if (!Objects.equals(this.autor, other.autor)) {
+            return false;
+        }
+        return Objects.equals(this.color, other.color);
+    }
+
+    @Override
+    public int compareTo(Libro o) {
+        return this.nombre.compareToIgnoreCase(o.nombre);
+    }
+    
+    
+
+}
--- src/main/java/ordenacionbusqueda/OrdenacionBusqueda.java
+++ src/main/java/ordenacionbusqueda/OrdenacionBusqueda.java
@@ -20,30 +20,54 @@ public class OrdenacionBusqueda {
         paises.add(new Pais("España", 46, 505_400, "Euro"));
         paises.add(new Pais("Portugal", 23, 25_400, "Euro"));
         paises.add(new Pais("EEUU", 146, 1_505_400, "Dolar Americano"));
-        paises.add(new Pais("Italia", 46, 505_400, "Euro"));
+        paises.add(new Pais("Vietnam", 46, 505_400, "Euro"));
         
         paises.forEach(System.out::println);
         System.out.println("--------------------------------------------------");
         
         // Oredenar por moneda, usando orden natural, requiere que Pais implemente Comparable
         Collections.sort(paises);
-        
         paises.forEach(System.out::println);
         
         //ordenacion usando comparator (proporcionamos el criterio 
         //mediante una lamda)
         Comparator<Pais> criterioSuperficie = (p1,p2)->Long.compare(p1.getSuperficie(), p2.getSuperficie());
         Comparator<Pais> criterioPoblacion = (p1,p2)->Long.compare(p1.getPoblacion(), p2.getPoblacion());
-        //Comparator<Pais> criterioNombre = (p1,p2)->p1.getNombre(p1.getNombre(), p2.getNombre());
-        Collections.sort(paises, criterioSuperficie);
+        Comparator<Pais> criterioNombre = (p1,p2)-> p1.getNombre().compareToIgnoreCase(p2.getNombre());
+        
+        Collections.sort(paises, criterioSuperficie.thenComparing(criterioNombre));
         System.out.println("--------------------------------------------------");
         paises.forEach(System.out::println);
         
-        //Collections.sort(paises, criterioSuperficie.thenComparing(criterioNombre));
-        System.out.println("--------------------------------------------------");
+        // busqueda por orden natural (por nombre)
+        //la lista donde buscar la información debe estar ordenada previamente 
+        //segun el criterio de orden natural (nombre) --> Comparable en Pais
+        Collections.sort(paises);
+        System.out.println("---------------------Busqueda binaria--------------------");
+        System.out.println("Lista ordenada según orden natural (nombre)--------------");
         paises.forEach(System.out::println);
+        Pais objetoBuscar = new Pais();
+        objetoBuscar.setNombre("Portugal"); //Lista ordenada por nombre, busqueda por nombre, key es nombre
+        int posicion = Collections.binarySearch(paises, objetoBuscar);
+        System.out.println("Portugal está en la posición " + posicion);
         
-        //Collections.sort(paises, criterioSuperficie);
+        //si intento buscar en la lista con un valor que no es nombre se puede obtener
+        //una posición erronea
+        //si ordeno la lista por un criterio y busco por otro, el resultado es inesperado
+        posicion = Collections.binarySearch(paises, objetoBuscar, criterioPoblacion);
+        System.out.println(posicion);
+        
+        System.out.println("--------Ordenación y busqueda por población--------------");
+        //antes de usar binarySearch por poblacion hay que ordenar la lista por poblacion
+        Collections.sort(paises, criterioPoblacion);
+        paises.forEach(System.out::println);
+        objetoBuscar = new Pais();
+        objetoBuscar.setPoblacion(146);
+        posicion = Collections.binarySearch(paises, objetoBuscar, criterioPoblacion);
+        
+        System.out.println("El pais que tiene 146 millones de habitantes es " + paises.get(posicion));
+        
+        
         
     }
     

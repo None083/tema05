004b535091dc1f4ea7508eeb52034f83c7cdc57a
Noelia <npergon914@g.educaand.es>
2023-02-05 13:50:31 +0100

    050223

--- src/main/java/ut05estructurasdealmacenamiento/UT0526Pila.java
+++ src/main/java/ut05estructurasdealmacenamiento/UT0526Pila.java
@@ -4,9 +4,11 @@
  */
 package ut05estructurasdealmacenamiento;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author noelia
+ * @author FX506
  * 
  * Implementa una Pila (Stack, en inglés) usando clases genéricas. 
  * Una pila es un almacén de datos en el que se sigue una política específica 
@@ -42,8 +44,48 @@ package ut05estructurasdealmacenamiento;
  *
  * 
  */
-public class UT0526 {
+public class UT0526Pila {
+
+    private ArrayList<Object> pila;
+    private int tamanio = 5;
+
+    public UT0526Pila() {
+        this.pila = new ArrayList<>();
+    }
+
+    private void apilar(Object o) {
+        if(this.pila.size() < this.tamanio){
+            this.pila.add(o);
+        }
+    }
     
+    private void desapilar(){
+        this.pila.remove(this.pila.size() - 1);
+    }
     
+    private boolean estaVacia(){
+        return this.pila.isEmpty();
+    }
     
+    private boolean estaLlena(){
+        return this.pila.size() >= this.tamanio;
+    }
+    
+    private int numeroElementos(){
+        return this.pila.size();
+    }
+    
+    private void mostrarElementos(){
+        for (int i = this.pila.size() - 1; i >= 0; i--) {
+            System.out.println(this.pila.get(i));
+        }
+    }
+    
+    private void rellenar(Object[] array){
+        for (int i = 0; i < array.length; i++) {
+            this.pila.add(array[i]);
+            this.tamanio = this.pila.size();
+        }
+    }
+
 }
---
+++ src/main/java/ut05estructurasdealmacenamiento/UT0527.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ut05estructurasdealmacenamiento;
+
+/**
+ *
+ * @author noelia
+ * 
+ * Crea una clase main() para probar la clase Pila, creando dos objetos tipo Pila, 
+ * uno almacenará String (Pila<String>) y el otro objetos de tipo Cancion (Pila<Cancion>). 
+ * Llama a los distintos métodos de los objetos de tipo Pila y prueba su funcionamiento.
+ *
+ */
+public class UT0527 {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
